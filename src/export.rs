34828aa83ef131809007ca7191d02ab8b8112ed0
Zach Stone <zach@zerovolts.com>
2019-10-24 00:57:48 -0400

    Add export tests

--- src/export.rs
+++ src/export.rs
@@ -31,12 +31,41 @@ impl WasmEncode for ExportSection {
         for export in self.0.iter() {
             let name = export.name.as_str();
             encoder.push_u8(name.len() as u8);
-            encoder.push_str(name);
+            byte_count += encoder.push_str(name) + 3;
             encoder.push_u8(export.desc.export_type as u8);
             encoder.push_u8(export.desc.index);
-            byte_count += name.len() as u8 + 3;
         }
         encoder.write_length(byte_count);
         byte_count + 2
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_section_encoding() {
+        let mut encoder = WasmEncoder::new();
+        let export_section = ExportSection(vec![Export {
+            name: "add".to_owned(),
+            desc: ExportDesc {
+                export_type: ExportType::FunctionIndex,
+                index: 255,
+            },
+        }]);
+        let byte_count = export_section.encode(&mut encoder);
+        let expected_bytes = [
+            0x07, // section id
+            0x07, // byte count
+            0x01, // export count
+            0x03, // name length
+            0x61, 0x64, 0x64, // name ("add")
+            0x00, // export type id
+            0xff, // export index
+        ];
+
+        assert_eq!(encoder.as_slice(), expected_bytes);
+        assert_eq!(byte_count, expected_bytes.len() as u8);
     }
 }
